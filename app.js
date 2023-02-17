06a03cee00622e80efa2fa75b8f2d9e088fa9669
guttapudi <guttapudi.karthik@gmail.com>
2023-02-17 20:45:54 +0800

    uploaded required files

---
+++ app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql2');
+
+app.use(bodyParser.json());
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', /* MySQL User */
+  password: 'rootuserpwd', /* MySQL Password */
+  database: 'full-stack-ecommerce' /* MySQL Database */
+});
+
+
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected with App...');
+});
+
+function apiResponse(results){
+    return JSON.stringify({"status": 200, "error": null, "response": results});
+}
+
+/**
+ * paste the API endpoints code below
+ */
+
+//API endpoint to GET all fruits
+app.get('/api/fruits',(req, res) => {
+    let sqlQuery = "SELECT * FROM fruitcompany.fruits;";
+   let query = conn.query(sqlQuery, (err, results) => {
+        if(err) throw err;
+        res.setHeader('Content-Type', 'application/json'); 
+        res.status(200);
+        res.send(apiResponse(results)); 
+    }); 
+});
+
+//API endpoint to GET details of a fruit
+app.get('/api/fruits/:name',(req, res) => {
+    let sqlQuery = "SELECT * FROM fruitcompany.fruits WHERE name='" + req.params.name+"'";
+    let query = conn.query(sqlQuery, (err, results) => {
+      if(err) throw err;
+      res.setHeader('Content-Type', 'application/json'); 
+      res.status(200);
+      res.send(apiResponse(results));
+    });
+  });
+
+//API endpoint to ADD a new fruit
+app.post('/api/fruits',(req, res) => {
+    let sqlQuery = `INSERT INTO fruitcompany.fruits VALUES ('${req.body.name}', '${req.body.quantity}', '${req.body.price}');`;
+    let query = conn.query(sqlQuery,(err, results) => {
+      if(err) throw err;
+      res.setHeader('Content-Type', 'application/json'); 
+      res.status(201);
+      res.send(apiResponse({'name': req.body.name, 'quantity': req.body.quantity, 'price': req.body.price}));
+    });
+  });
+
+//API endpoint to UPDATE an existing fruit
+  app.put('/api/fruits/:name',(req, res) => {
+    let sqlQuery = "UPDATE fruitcompany.fruits SET quantity='"+req.body.quantity+"', price='"+req.body.price+"' WHERE name='"+req.params.name+"'";
+    let query = conn.query(sqlQuery, (err, results) => {
+      if(err) throw err;
+      res.setHeader('Content-Type', 'application/json'); 
+      res.status(201);
+      res.send(apiResponse({'name': req.params.name, 'quantity': req.body.quantity, 'price': req.body.price}));
+    });
+  });
+  
+//API endpoint to delete a fruit
+  app.delete('/api/fruits/:name',(req, res) => {
+    let sqlQuery = "DELETE FROM fruitcompany.fruits WHERE name='"+req.params.name+"'";
+    let query = conn.query(sqlQuery, (err, results) => {
+      if(err) throw err;
+      res.setHeader('Content-Type', 'application/json'); 
+      res.status(200);
+      res.send(apiResponse({"msg": "Fruit deleted"}));
+    });
+  });
+
+app.listen(3000,() =>{
+    console.log('Server started on port 3000...');
+  });
